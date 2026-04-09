d1d56447fdb0440e762568296521a825efd70132
rferreira1990 <rfpav.ric@gmail.com>
2026-04-09 21:03:26 +0000

    9 de abr. de 2026, 22:03

--- routes/web.php
+++ routes/web.php
@@ -3,20 +3,14 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    return redirect()->route('admin.dashboard');
+});
 
-
-Route::middleware(['auth'])->group(function () {
-
-    Route::prefix('admin')->group(function () {
-
+Route::middleware(['auth'])
+    ->prefix('admin')
+    ->name('admin.')
+    ->group(function () {
         Route::get('/dashboard', function () {
             return view('admin.dashboard.index');
         })->name('dashboard');
-
     });
-
-});
-
-
-  return redirect()->route('dashboard');
-});
