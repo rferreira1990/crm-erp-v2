cd1baf0f2bec53b4585ddbba29dd6b657f8af155
rferreira1990 <rfpav.ric@gmail.com>
2026-04-09 20:42:48 +0000

    9 de abr. de 2026, 21:42

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,20 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+
+
+Route::middleware(['auth'])->group(function () {
+
+    Route::prefix('admin')->group(function () {
+
+        Route::get('/dashboard', function () {
+            return view('admin.dashboard.index');
+        })->name('dashboard');
+
+    });
+
+});
+
+
     return view('welcome');
 });
